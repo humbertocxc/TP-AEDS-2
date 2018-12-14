bcb5ef20fd0a4a4c95e2be28cd690df5e60ba804
humbertocxc <38592471+humbertocxc@users.noreply.github.com>
2018-12-14 19:21:59 -0200

    Add files via upload

--- funcao.c
+++ funcao.c
@@ -4,63 +4,108 @@
 #include <ctype.h>
 #include "funcao.h"
 
-/*função que cria a hash recebendo a palavra e o tamanho, e retorna a posição na tabela, que é equivalente
-ao resto da divisão do valor inteiro da palavra por 256(tamanho de um char)*/
 unsigned int fHash(char* str, unsigned int len){
-	const unsigned int resp = 2;
-	unsigned int hash = 0;
-	unsigned int i = 0;
-	for (int i = 0; i < len; i++){
-		hash *= resp;
-		hash ^= (*str);
-	}
-	return hash%256;
+  const unsigned int resp = 2;
+  unsigned int hash = 0;
+  unsigned int i = 0;
+  for (int i = 0; i < len; i++){
+    hash *= resp;
+    hash ^= (*str);
+  }
+  return hash%256;
+}
+
+void separa_palavras(FILE *IN){
+  char ch, palavra[20];
+  int i = 0, hash = 0, ct=0;
+  //aqui todo o arquivo será rodado, criando strings de letras maiusculas e minúsculas
+  while ((ch = getc(IN)) != EOF){
+  	if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
+  	  palavra[ct] = ch;
+  	  ct++;
+  	}else{
+    	palavra[ct] = '\0';	
+      //testa tamanho pois a ideia é aceitar apenas palavras de 2 letras ou mais
+      if(strlen(palavra) > 1){
+        //deixa todas as letras da palavra minúsculas
+        for(i = 0; palavra[i]; i++) palavra[i] = tolower(palavra[i]);
+        //chamando a função que retorna uma posição na tabela hash
+        hash = fHash(palavra, strlen(palavra));
+        //usa a posição na tabela recebida para testar se há colisões, e se há palavras repetidas
+        teste_colisao(palavra, hash);
+      }
+      ct=0;
+  	}
+  }
+}
+
+void teste_colisao(char palavra[], int hash){
+  int aux = hash, i;
+  //testa se a posição na tabela está vazia, para introduzir a palavra
+  if(ht[hash].repeticao == 0){
+    strcpy(ht[hash].palavra, palavra);
+    ht[hash].repeticao ++;
+  //se houver colisão e a palavra for igual, soma 1 no nmr de repetições
+  }else if(strcmp(ht[hash].palavra,palavra) == 0){
+    ht[hash].repeticao ++;
+  //caso a chave seja 0, procura uma posição vazia na tabela e coloca a palavra e depois soma 1 na posição onde a chave era 0
+  }else if (ht[hash].chave == 0){
+    while (ht[hash].repeticao != 0){
+      hash ++;
+    }
+    strcpy(ht[hash].palavra, palavra);
+    ht[hash].repeticao ++;
+    ht[aux].chave ++;
+  //caso a chave seja diferente de 0, ele procura uma posição com a palavra igual
+  }else if(ht[hash].chave != 0){
+    for (i = 0;i < 256; i++){
+      if (strcmp(ht[i].palavra,palavra) == 0){
+        ht[i].repeticao ++;
+        break;
+      }
+    }
+  }
 }
 
 void ordena(){
-
-	//ordena por quantidade
-  	for (int i = 0; i < 255; i++){
-		int max = i;
-		for (int j = (i+1); j < 256; j++){
-			if(ht[j].repeticao > ht[max].repeticao)
-				max = j;
-		}
-		if (i != max){
-			strcpy(htaux[i].palavra, ht[i].palavra);
-			int flag = ht[i].repeticao;
-			strcpy(ht[i].palavra, ht[max].palavra);
-			ht[i].repeticao = ht[max].repeticao;
-			strcpy(ht[max].palavra,htaux[i].palavra);
-			ht[max].repeticao = flag;
-		}
-	}
-
-	//ordena por ordem alfabetica
-  	for (int i = 0; i < 256; i++){
-    	for (int j = 0; j < 256; j++){
-
-    		//Seleciona apenas as de mesma quantidade
-      		if (ht[i].repeticao == ht[j].repeticao){
-
-        		//A função strcoll compara letra por letra em uma string, até que uma delas seja menor
-        		if(strcoll(ht[i].palavra,ht[j].palavra) < 0){
-	            	strcpy(htaux[j].palavra, ht[j].palavra);
-	    			strcpy(ht[j].palavra, ht[i].palavra);
-	    			strcpy(ht[i].palavra,htaux[j].palavra);
-        		}
-    	    }
-    	}
-  	}
+  //ordena por quantidade
+  for (int i = 0; i < 255; i++){
+    int max = i;
+    for (int j = (i+1); j < 256; j++){
+      if(ht[j].repeticao > ht[max].repeticao)
+        max = j;
+    }
+    if (i != max){
+      strcpy(htaux[i].palavra, ht[i].palavra);
+      int flag = ht[i].repeticao;
+      strcpy(ht[i].palavra, ht[max].palavra);
+      ht[i].repeticao = ht[max].repeticao;
+      strcpy(ht[max].palavra,htaux[i].palavra);
+      ht[max].repeticao = flag;
+    }
+  }
+    //ordena por ordem alfabetica
+    for (int i = 0; i < 256; i++){
+      for (int j = 0; j < 256; j++){
+      //Seleciona apenas as de mesma quantidade
+      if (ht[i].repeticao == ht[j].repeticao){
+        //A função strcoll compara letra por letra em uma string, até que uma delas seja menor
+        if(strcoll(ht[i].palavra,ht[j].palavra) < 0){
+          strcpy(htaux[j].palavra, ht[j].palavra);
+          strcpy(ht[j].palavra, ht[i].palavra);
+          strcpy(ht[i].palavra,htaux[j].palavra);
+        }
+      }
+    }
+  }
 }
 
 void imprime(){
-
-	//imprime todas as palavras com mais de 2 repetições
-	for (int i = 0; i < 256; i++){
-		if (ht[i].repeticao >= 2){
-      		printf ("%d ", ht[i].repeticao);
-			printf ("%s\n", ht[i].palavra);
-		}
-	}
+  //imprime todas as palavras com mais de 2 repetições
+  for (int i = 0; i < 256; i++){
+    if (ht[i].repeticao >= 2){
+      printf ("%d ", ht[i].repeticao);
+      printf ("%s\n", ht[i].palavra);
+    }
+  }
 }
--- main.c
+++ main.c
@@ -1,89 +1,20 @@
 #include "funcao.h"
 
 int main(int argc, char **argv){
-  char ch, palavra[20];
-  int i = 0, hash = 0, aux = 0, j, ct=0;
-
   FILE *IN;
   IN = fopen (argv[1],"r");
   if(IN == NULL){
     printf("Arquivo não foi aberto!\n");
     exit (1);
   }
-
-  //zeramos as variáveis das structs
-  for (i = 0; i < 256; i++){
+  //zera as variáveis das structs
+  for (int i = 0; i < 256; i++){
     ht[i].repeticao = 0;
     ht[i].chave = 0;
   }
-
   fflush(stdout);
-
-  //aqui todo o arquivo será rodado, separando por palavras, e criando uma heap em cada palavra
-  while ((ch = getc(IN)) != EOF){
-  	if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z')){
-  	  palavra[ct] = ch;
-  	  ct++;
-  	}else{
-    	palavra[ct] = '\0';	
-      //if para aceitar apenas palavras de 2 letras ou mais
-      if(strlen(palavra) > 1){
-        //deixa todas as letras da palavra minúsculas
-        for(i = 0; palavra[i]; i++) palavra[i] = tolower(palavra[i]);
-
-        //chamando a função que cria a hash, e retorna uma variável do tipo hash
-        hash = fHash(palavra, strlen(palavra));
-        aux = hash;
-
-        //se no lugar de inserção da palavra não houver nenhum índice de repetição, coloca ela no mesmo
-        if(ht[hash].repeticao == 0){
-          strcpy(ht[hash].palavra, palavra);
-          ht[hash].repeticao ++;
-
-        //se a palavra for igual, soma 1 na repetição
-        }else if(strcmp(ht[hash].palavra,palavra) == 0){
-          ht[hash].repeticao ++;
-
-        //se a palavra for diferente, ele continua fazendo o mesmo até não encontrar um índice de repetição
-        }else if (ht[hash].chave == 0){
-          while (ht[hash].repeticao != 0){
-            hash ++;
-          }
-          strcpy(ht[hash].palavra, palavra);
-          ht[hash].repeticao ++;
-
-        //somar +1 na chave do aux para ver se exise alguma colisão
-          ht[aux].chave ++;
-        }else if(ht[hash].chave != 0){
-
-        //procurar alguma palavra igual no vetor
-          for (i = 0;i < 256; i++){
-            if (strcmp(ht[i].palavra,palavra) == 0){
-
-              //quando for igual, soma 1 nas repetições e sai do loop
-              ht[i].repeticao ++;
-              break;
-            }
-          }
-
-        //caso o valor seja o mesmo mas a palavra for diferente, ele procura uma nova posição
-        }else{
-          while (ht[hash].chave != 0){
-            hash ++;
-          }
-
-          //quando encontrar uma posição vazia, adiciona a palavra e soma 1 nas repetições
-          strcpy(ht[hash].palavra, palavra);
-          ht[hash].chave ++;
-        }
-      }
-      ct=0;
-  	}
-  }
-
-  //Chama função ordena "funcoes.h"
+  separa_palavras(IN);
   ordena();
-  //Chama função imprime "funcoes.h"
 	imprime();
   fclose (IN);
   return 0;
